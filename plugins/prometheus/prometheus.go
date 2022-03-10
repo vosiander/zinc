494a608470495273f9ae12d7eee6eb495acf818f
vosiander <info@veitosiander.de>
2022-03-10 13:54:30 +0100

    add prometheus gauge

--- plugins/prometheus/prometheus.go
+++ plugins/prometheus/prometheus.go
@@ -15,6 +15,7 @@ type (
 		logger      *logrus.Entry
 		conf        Config
 		incCounters map[string]prometheus.Counter
+		incGauges   map[string]prometheus.Gauge
 	}
 
 	Config struct {
@@ -88,13 +89,30 @@ func (p *Plugin) AddCounter(name string, count float64, help string) {
 	}
 
 	if _, isOK := p.incCounters[name]; !isOK {
-		inc := promauto.NewCounter(prometheus.CounterOpts{
-			Name: name,
-			Help: help,
-		})
+		inc := promauto.NewCounter(prometheus.CounterOpts{Name: name, Help: help})
 		l.WithField("name", name).Debug("init metrics counter")
 
 		p.incCounters[name] = inc
 	}
 	p.incCounters[name].Add(count)
+}
+
+func (p *Plugin) AddGauges(name string, gauge float64, help string) {
+	if !p.conf.Enable {
+		return
+	}
+
+	l := p.logger
+	if name == "" {
+		l.Warn("incGauge metric with empty name. skipping. ")
+		return
+	}
+
+	if _, isOK := p.incGauges[name]; !isOK {
+		inc := promauto.NewGauge(prometheus.GaugeOpts{Name: name, Help: help})
+		l.WithField("name", name).Debug("init metrics counter")
+
+		p.incGauges[name] = inc
+	}
+	p.incGauges[name].Add(gauge)
 }
