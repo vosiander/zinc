e71e5a44d067aff68a38bfecf39d9b0cd640591a
veit osiander <git@veitosiander.de>
2023-01-09 00:24:23 +0100

    add config file switch [patch]

--- core/options.go
+++ core/options.go
@@ -4,9 +4,41 @@ type (
 	Option func(c *Core, conf interface{}) error
 )
 
+func LoadConfigFromEnvironment(yamlFile string, service string, url string) Option {
+	return func(c *Core, conf interface{}) error {
+		l := c.Logger().WithFields(map[string]any{
+			"component": "LoadConfigFromEnvironment",
+			"yaml":      yamlFile,
+			"service":   service,
+			"url":       url,
+		})
+
+		if yamlFile != "" {
+			err := c.yl.LoadYamlConfig(yamlFile, conf)
+			if err == nil {
+				l.Tracef("%#v", conf)
+				return nil
+			}
+			l.WithError(err).Warn("error loading yaml config")
+		}
+
+		if service != "" && url != "" {
+			err := c.cl.LoadConfig(url, service, conf)
+			if err == nil {
+				l.Tracef("%#v", conf)
+				return nil
+			}
+			l.WithError(err).Warn("error loading yaml config")
+		}
+
+		l.Fatal("could not load configs from either file or configurator")
+		return nil
+	}
+}
+
 func LoadYamlConfig(filename string) Option {
 	return func(c *Core, conf interface{}) error {
-		l := c.Logger().WithField("component", "LoadFromConfig")
+		l := c.Logger().WithField("component", "LoadFromYamlConfig")
 
 		if err := c.yl.LoadYamlConfig(filename, conf); err != nil {
 			l.WithError(err).Fatal("error loading yaml config")
